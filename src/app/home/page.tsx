8b7ce4023da82e4dddd0315fe0967dab18d8a560
FxFurEN <nikitostimoshenko@gmail.com>
2024-08-18 15:07:40 +0300

    add home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,12 @@
-const Home = () => {
-	return <div>Home</div>
-}
+// pages/index.tsx
+import { fetchCases } from '@/lib/db'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import Home from './page.client'
 
-export default Home
+export default async function HomePage() {
+	const session = await getServerSession(authOptions)
+	const cases = await fetchCases()
+
+	return <Home userId={session?.user.id} cases={cases} />
+}
