432038071e105c759065cbe89b30903eca062409
FxFurEN <nikitostimoshenko@gmail.com>
2024-08-18 15:07:16 +0300

    add db func lib

---
+++ src/lib/db.ts
@@ -0,0 +1,64 @@
+import { Backend_URL } from './Constants'
+
+export type Case = {
+	id: string
+	name: string
+}
+
+export const fetchCases = async (): Promise<Case[]> => {
+	const res = await fetch(`${Backend_URL}/cases`, {
+		method: 'GET',
+	})
+
+	if (!res.ok) {
+		throw new Error('Failed to fetch cases')
+	}
+
+	return res.json()
+}
+
+export const createCase = async (
+	name: string,
+	authorId: string
+): Promise<Case> => {
+	const res = await fetch(`${Backend_URL}/cases`, {
+		method: 'POST',
+		body: JSON.stringify({
+			name,
+			authorId,
+		}),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+
+	if (!res.ok) {
+		throw new Error('Failed to create case')
+	}
+
+	return res.json()
+}
+
+export const deleteCase = async (id: string): Promise<void> => {
+	const res = await fetch(`${Backend_URL}/cases/${id}`, {
+		method: 'DELETE',
+	})
+
+	if (!res.ok) {
+		throw new Error('Failed to delete case')
+	}
+}
+
+export const updateCase = async (id: string, name: string): Promise<void> => {
+	const res = await fetch(`${Backend_URL}/cases/${id}`, {
+		method: 'PATCH',
+		body: JSON.stringify({ name }),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+
+	if (!res.ok) {
+		throw new Error('Failed to update case')
+	}
+}
